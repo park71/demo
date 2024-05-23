9ae85c2885089e4e6a9b7e3dfe9c0619ef03c0c8
park <seonghoopark71@gmail.comasdgit config --listgit config --global user.email seonghoopark71@gmail.com>
2024-05-23 14:32:06 +0900

    Initial commit

---
+++ src/test/java/hello/spaproject/SpaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
